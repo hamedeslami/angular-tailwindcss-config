0426974ccf1ba6db0d3be9977a803c81557404f3
Hamed Eslami <h.eslami@behsacorp.com>
2025-10-14 16:29:29 +0330

    refactoe login page

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,8 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ILoginForm } from '@features/auth/models/login.model';
 import { AuthTextService } from '@features/auth/services/auth-text.service';
 import { Button } from '@shared/components/button/button';
 import { FloatingLabel } from '@shared/components/floating-label/floating-label';
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -10,41 +13,29 @@ import { FloatingLabel } from '@shared/components/floating-label/floating-label'
   templateUrl: './login.html',
 })
 export class Login {
-  loading = signal(false);
-  form!: FormGroup;
-
+  readonly loading = signal(false);
+  readonly form: FormGroup<ILoginForm>;
   private textService = inject(AuthTextService);
-  texts = this.textService.getTexts('login');
+  readonly texts = this.textService.getTexts('login');
+  readonly isLoading = computed(() => this.loading());
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      username: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(/^[^\u0600-\u06FF\s]+$/),
-        ],
-      ],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(/^[^\u0600-\u06FF\s]+$/),
-        ],
-      ],
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.nonNullable.group({
+      username: ['', [Validators.required, Validators.pattern(/^[^\u0600-\u06FF\s]+$/)]],
+      password: ['', [Validators.required, Validators.pattern(/^[^\u0600-\u06FF\s]+$/)]],
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
       return;
     }
 
-    console.log(this.form.value);
-  }
-
-  isLoading(): boolean {
-    return false;
+    const { username, password } = this.form.getRawValue();
+    this.loading.set(true);
+    console.log({ username, password: '••••••' });
   }
 }
