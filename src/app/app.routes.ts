23d6a9fb53fe7aad25eb74658e921ed15864d5f7
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-11 18:34:19 +0330

    add not-found and no-access route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,24 @@ import { AuthGuard } from '@core/guards/auth.guard';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
         path: 'auth',
         canActivate: [AuthGuard],
         loadChildren: () =>
             import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
     },
+    {
+        path: 'no-access',
+        loadChildren: () =>
+            import('./features/no-access/no-access.routes').then(m => m.NO_ACCESS_ROUTES),
+    },
+    {
+        path: '**',
+        loadChildren: () =>
+            import('./features/not-found/not-found.routes').then(m => m.NOT_FOUND_ROUTES),
+    }
 ];
