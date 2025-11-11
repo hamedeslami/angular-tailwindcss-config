2e8740bdf24724032360d70283643db646b3c1ef
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-11 13:45:12 +0330

    add guards to auth route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: 'auth',
+        canActivate: [AuthGuard],
         loadChildren: () =>
             import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
     },
