7c53bbfc09aa1f5fef5aaae85fff4dc84f52884e
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-11 13:47:41 +0330

    fixed some issue and add en lang

--- src/app/core/services/error-handler.service.ts
+++ src/app/core/services/error-handler.service.ts
@@ -1,37 +1,41 @@
-import { Injectable } from '@angular/core';
+import { computed, inject, Injectable } from '@angular/core';
 import { ToastService } from './toast.service';  // مسیر ToastService
+import { LangService } from './lang.service';
+import { ERROR_HANDLER_TEXTS} from '@core/content/error-handler.text';
+import { TextService } from './text.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
-
-  constructor(private toastService: ToastService) {}
+  private textService = inject(TextService);
+  private toastService = inject(ToastService);
+  readonly texts = this.textService.getTexts(ERROR_HANDLER_TEXTS, 'httpCode');
 
   handleError(errorCode: number, errorMessage: string): void {
     let message = '';
 
     switch (errorCode) {
       case 400:
-        message = 'درخواست شما معتبر نیست. لطفاً دوباره تلاش کنید.';
+        message = this.texts()['400'];
         break;
       case 401:
-        message = 'شما وارد حساب کاربری خود نشده‌اید یا توکن منقضی شده است. لطفاً وارد شوید.';
+        message = this.texts()['401'];
         break;
       case 403:
-        message = 'دسترسی شما محدود است. شما اجازه دسترسی به این صفحه را ندارید.';
+        message = this.texts()['403'];
         break;
       case 404:
-        message = 'صفحه یا منبع مورد نظر یافت نشد.';
+        message = this.texts()['404'];
         break;
       case 500:
-        message = 'خطای سرور. لطفاً دوباره تلاش کنید.';
+        message = this.texts()['500'];
         break;
       case 503:
-        message = 'سرور در دسترس نیست. لطفاً بعداً تلاش کنید.';
+        message = this.texts()['503'];
         break;
       default:
-        message = errorMessage || 'خطای ناشناخته، لطفاً بعداً تلاش کنید.';
+        message = errorMessage || this.texts()['default'];
     }
 
     this.toastService.show(message, 'error');
@@ -39,7 +43,7 @@ export class ErrorHandlerService {
 
   handleResponseError(response: any): void {
     if (response && response.responseCode && response.responseCode !== 200) {
-      this.toastService.show(response.message || 'خطای ناشناخته در پردازش درخواست');
+      this.toastService.show(response.message || this.texts()['default']);
     }
   }
 }
