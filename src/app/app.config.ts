d2f0ed18d1b987dc110158d0dc34f78f7aa9b0b0
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-11 13:44:32 +0330

    add http client provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { LangService } from './core/services/lang.service';
 import { LangTitleStrategy } from '@core/strategies/lang-title.strategy';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from '@core/interceptors/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideAppInitializer(() => {
       const langService = inject(LangService);
       return langService.init();
