c5bfa428f734c5c6c60629c018bb7495ae7964f5
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-15 18:05:05 +0330

    create user dropdown to dashbaord header

---
+++ src/app/features/dashboard/layout/header/components/user-dropdown/user-dropdown.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Dropdown } from '@shared/components/dropdown/dropdown';
+import { TextService } from '@core/services/text.service';
+import { LAYOUT_TEXTS } from '@features/dashboard/content/layout.text';
+import { ThemeService } from '@core/services/theme.service';
+import { LangService } from '@core/services/lang.service';
+
+
+@Component({
+  selector: 'app-user-dropdown',
+  templateUrl: './user-dropdown.html',
+  imports:[CommonModule,RouterModule,Dropdown]
+})
+export class UserDropdown {
+  isOpen = false;
+
+  private textService = inject(TextService);
+  readonly texts = this.textService.getTexts(LAYOUT_TEXTS, 'user');
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
+}
