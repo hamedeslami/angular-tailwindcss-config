8902a092a1d577d7acf1b4ebdead2b9792e6a32f
Hamed Eslami <h.eslami@behsacorp.com>
2025-11-22 18:10:39 +0330

    fixed dark mode issue

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,15 +1,16 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component, Input, Output, EventEmitter, input, output } from '@angular/core';
 import { Spinner } from '../spinner/spinner';
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule, Spinner],
+  imports: [Spinner, NgClass],
   templateUrl: './button.html',
 })
 export class Button {
   variant = input<'contained' | 'outline' | 'text'>('contained');
   icon = input<string>();
+  className = input<string>('');
   disabled = input<boolean>(false);
   loading = input<boolean>(false);
   onClick = output<MouseEvent>();
